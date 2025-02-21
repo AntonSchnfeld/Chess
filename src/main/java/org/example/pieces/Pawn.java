62890f1307a7efaa511a69fb0a6992af0c29c1f0
SchnfeldDev <schnfeldanton@gmail.com>
2025-02-21 21:07:48 +0100

    Added most of Pawn.getValidMoves implementation and MoveHistory as well as SimpleMoveHistory to record mvoes.

--- src/main/java/org/example/pieces/Pawn.java
+++ src/main/java/org/example/pieces/Pawn.java
@@ -1,17 +1,45 @@
 package org.example.pieces;
 
-import org.example.ChessBoardView;
-import org.example.MoveCollection;
-import org.example.Position;
+import org.example.*;
 
 public class Pawn extends ChessPiece {
+    private boolean moved;
 
     public Pawn(boolean colour) {
         super(colour);
+        this.moved = false;
+    }
+
+    public boolean hasMoved() {
+        return moved;
     }
 
     @Override
     public MoveCollection getValidMoves(ChessBoardView view, Position curPos) {
-        return new MoveCollection();
+        // TODO: En passant and promotion
+
+        MoveCollection moves = new MoveCollection();
+        int direction = colour ? 1 : -1;
+        ChessBoardBounds bounds = view.getChessBoardBounds();
+
+        Position inFront = curPos.offset(0, direction);
+        if (bounds.contains(inFront) && view.getPieceAt(inFront) == null) {
+            moves.add(Move.normal(curPos, inFront, this));
+            Position twoInFront = inFront.offset(0, direction);
+            if (bounds.contains(twoInFront) && view.getPieceAt(twoInFront) == null && !moved)
+                moves.add(Move.normal(curPos, twoInFront, this));
+        }
+
+        Position leftDiagonal = curPos.offset(direction, -1);
+        ChessPiece leftDiagonalPiece = view.getPieceAt(leftDiagonal);
+        if (leftDiagonalPiece != null && leftDiagonalPiece.getColour() != colour)
+            moves.add(Move.of(curPos, leftDiagonal, this, leftDiagonalPiece, false));
+
+        Position rightDiagonal = curPos.offset(direction, 1);
+        ChessPiece rightDiagonalPiece = view.getPieceAt(rightDiagonal);
+        if (rightDiagonalPiece != null&& rightDiagonalPiece.getColour() != colour)
+            moves.add(Move.of(curPos, rightDiagonal, this, rightDiagonalPiece, false));
+
+        return moves;
     }
 }
